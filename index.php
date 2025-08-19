339bff997aa29c5d86516fd68b2a916700a8ad83
Arsen Movsesjan <134381057+pragmaticLudusian@users.noreply.github.com>
2025-08-19 15:39:12 +0300

    feature: add db, index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>DnD Character Sheet Manager</title>
+</head>
+
+<body>
+  <form action="createDB.php" method="GET">
+    <p>Database not found! Create one?</p>
+    <input type="submit" value="Create DB">
+  </form>
+  <?php
+  /* TODO
+    create db "DnD"
+    then create table "Characters" with Name, Class, STR, DEX, CON, INT, WIS, CHR
+  */
+  ?>
+</body>
+
+</html>
