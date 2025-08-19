2e50ed0c44a78caaccd8153d288f04ec4f5df85d
Arsen Movsesjan <134381057+pragmaticLudusian@users.noreply.github.com>
2025-08-19 17:12:20 +0300

    feature: detect if db exists

--- index.php
+++ index.php
@@ -8,16 +8,29 @@
 </head>
 
 <body>
-  <form action="db-create.php" method="GET">
-    <p>Database not found! Create one?</p>
-    <input type="submit" value="Create DB">
-  </form>
   <?php
   /* TODO
-    find db and table:
-      if found, redir to manage characters
-      else, show prompt to create the db
+    be able to list all chars (or none if there are none)
+    add the name, class and stats using a form and submit to add to db
+    build search interface using ajax
   */
+  include 'db-conf.php';
+  $con = mysqli_connect(HOST, USER, PASS)
+    or die("Connection Error" . mysqli_error($con));
+  mysqli_set_charset($con, "utf8");
+  $sql = "SELECT schema_name FROM information_schema.schemata WHERE schema_name='dnd'";
+  $res = mysqli_query($con, $sql);
+  if (!mysqli_num_rows($res)) { // if db doesn't exist
+    echo '
+      <form action="db-create.php" method="GET">
+        <p>Database not found! Create one?</p>
+        <input type="submit" value="Create DB">
+      </form>
+    '; // can't handle `s
+  } else {
+    echo 'manage your own damn chars';
+  }
+  mysqli_close($con);
   ?>
 </body>
 
